deeb6e6e9bb865265b13309ad67e7147c0e6c89c
Robert Norlander <robertn@opera.com>
2012-02-21 18:25:21 +0100

    Fixed so that if the server fails to set the state it tries again

--- kaffe_checker/kaffe.py
+++ kaffe_checker/kaffe.py
@@ -9,15 +9,28 @@ class KaffeState:
 	OFF = 2
 
 def make_request(state):
-	connection = httplib.HTTPConnection('localhost')
+	connection = httplib.HTTPConnection('www.yawnmedia.se')
 
 	if state == KaffeState.ON:
 		print "Changing state to: ON"
-		connection.request("GET", "http://jeppe.linkoping.osa/kaffe/?state=ON")
+		connection.request("GET", "http://www.yawnmedia.se/kaffe/?state=ON")
 	elif state == KaffeState.OFF:
 		print "Changing state to: OFF"
-		connection.request("GET", "http://jeppe.linkoping.osa/kaffe/?state=OFF")
+		connection.request("GET", "http://www.yawnmedia.se/kaffe/?state=OFF")
 
+def get_server_state():
+	connection = httplib.HTTPConnection('www.yawnmedia.se');
+	connection.request("GET", "http://www.yawnmedia.se/kaffe/");
+	response = connection.getresponse();
+
+	state = response.read()
+
+	values = state.split(';')
+
+	if values[0] == "OFF":
+		return KaffeState.OFF
+	elif values[0] == "ON":
+		return KaffeState.ON
 
 def check_state():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -38,7 +51,11 @@ if __name__ == "__main__":
 		# if state has changed, let the server know
 		if new_state != last_state:
 			last_state = new_state
-			make_request(new_state)
+			request = make_request(new_state)
+
+
+		if get_server_state() != last_state:
+			request = make_request(last_state)
 
 		time.sleep(5)
 
